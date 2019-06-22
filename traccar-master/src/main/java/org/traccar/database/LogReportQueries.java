760138c9c9724fdf797b85833c12db999f40f5c0
sankalpbhojwani <sankalp.b@deeplogix.tech>
2019-06-22 17:40:14 +0530

    Complete testing.

--- traccar-master/src/main/java/org/traccar/database/LogReportQueries.java
+++ traccar-master/src/main/java/org/traccar/database/LogReportQueries.java
@@ -19,20 +19,23 @@ public class LogReportQueries {
     
     /**
      * Create logs of error in database
-     * @param identifier,Loq type,Log message
-     * @return integer, non zero if successfully added
+     * @param Identifier Each device has a specific identifier
+     * @param Log_type Type for  Log creation
+     * @param Log_message Message with respect to log type 
+     * @return Integer Non zero if successfully added
     */
     public static int createLog(String uniqueid,String log_type,String log_message)throws SQLException{
             Connection con=dataSource.getConnection();
-            String query1="select userid from tc_user_device where deviceid in (select id from tc_devices where uniqueid="+uniqueid+")";
-            Statement statement=con.createStatement();
-            ResultSet rs=statement.executeQuery(query1);
+            String query1="select userid from tc_user_device where deviceid in (select id from tc_devices where uniqueid=?)";
+            PreparedStatement preparedStatement=con.prepareStatement(query1);
+            preparedStatement.setString(1, uniqueid);
+            ResultSet rs=preparedStatement.executeQuery();
             int userid=0;
         if(rs.next()){
             userid=rs.getInt(1);
          }
             String query2="insert into logreport(log_type,log_message,userid) values(?,?,?)";
-            PreparedStatement preparedStatement=con.prepareStatement(query2);
+            preparedStatement=con.prepareStatement(query2);
             preparedStatement.setString(1, log_type);
             preparedStatement.setString(2, log_message);
             preparedStatement.setInt(3, userid);
