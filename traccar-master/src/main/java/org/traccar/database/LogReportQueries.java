7fb512008f162f38e9df16f65f5cb7f5057c4c87
sankalpbhojwani <sankalp.b@deeplogix.tech>
2019-06-27 18:49:12 +0530

    Get List of Logs associated with the user.

--- traccar-master/src/main/java/org/traccar/database/LogReportQueries.java
+++ traccar-master/src/main/java/org/traccar/database/LogReportQueries.java
@@ -9,7 +9,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.sql.DataSource;
+import org.traccar.model.LogReport;
 
 /**
  * Create logs in database for errors associated with unique identifier
@@ -31,9 +35,9 @@ public class LogReportQueries {
             preparedStatement.setString(1, uniqueid);
             ResultSet rs=preparedStatement.executeQuery();
             int userid=0;
-        if(rs.next()){
-            userid=rs.getInt(1);
-         }
+            if(rs.next()){
+                userid=rs.getInt(1);
+            }
             String query2="insert into logreport(log_type,log_message,userid) values(?,?,?)";
             preparedStatement=con.prepareStatement(query2);
             preparedStatement.setString(1, log_type);
@@ -42,10 +46,35 @@ public class LogReportQueries {
             int i=0;
             i=preparedStatement.executeUpdate();
             con.close();
-        if(i>0){
-            return i;
-        }else{
-            return 0;
-        }
+            if(i>0){
+                return i;
+            }else{
+                return 0;
+            }
+    }
+    
+    /**
+     * Get logs present in database
+     * @param UserId Unique id associated to user
+     * @return List<LogReport> List of log report 
+    */
+    public static List<LogReport> getLogsByUserId(int userid) throws SQLException{
+            List<LogReport> logReports = new ArrayList<>();
+            Connection con = dataSource.getConnection();
+            String getQuery = "select * from logreport where userid=?";
+            PreparedStatement preparedStatement = con.prepareStatement(getQuery);
+            preparedStatement.setInt(1, userid);
+            ResultSet resultSet = preparedStatement.executeQuery();
+             if(resultSet.next()){
+                 do{
+                     LogReport logReport =  new LogReport();
+                     logReport.setId(resultSet.getInt(1));
+                     logReport.setLog_message(resultSet.getString(2));
+                     logReport.setLog_type(resultSet.getString(3));
+                     logReport.setLogged_date(new Date(resultSet.getTimestamp(4).getTime()));
+                     logReports.add(logReport);
+                 }while(resultSet.next());
+     }
+     return logReports;
     }
 }
