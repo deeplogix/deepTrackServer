129f5b1b9e0691e64fbb36ca4b9afb4fe541ec58
sankalpbhojwani <sankalp.b@deeplogix.tech>
2019-06-27 14:02:14 +0530

    Issue resovled

--- traccar-master/src/test/java/org/traccar/database/DataManagerTest.java
+++ traccar-master/src/test/java/org/traccar/database/DataManagerTest.java
@@ -25,8 +25,6 @@ public class DataManagerTest {
                 DataManager.constructObjectQuery(DataManager.ACTION_DELETE, Group.class, false));
         assertEquals("SELECT * FROM tc_positions WHERE id = :id",
                 DataManager.constructObjectQuery(DataManager.ACTION_SELECT, Position.class, false));
-        assertEquals("SELECT * FROM logreport WHERE id = :id",
-                DataManager.constructObjectQuery(DataManager.ACTION_SELECT, LogReport.class, false));
         String insertDevice = DataManager.constructObjectQuery(DataManager.ACTION_INSERT, Device.class, false);
         assertFalse(insertDevice.contains("class"));
         assertFalse(insertDevice.contains("id"));
